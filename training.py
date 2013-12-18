1003b2ee0a214d071bd5a9b4309e766210692837
iehoshia <jepgez@gmail.com>
2013-12-18 08:11:05 -0600

    18 / 12 / 2013

--- training.py
+++ training.py
@@ -39,12 +39,16 @@ class TrainingCourseCategory(ModelView, ModelSQL):
     __name__ = 'training.course_category'
     
     name = fields.Char('Full Name')
+    description = fields.Text('Description',
+                                    translate=True,
+                                    help="Allows to the user to write the description of the course type")
     
 class TrainingCourseType(ModelView, ModelSQL):
     "The Course's Type"
     __name__ = 'training.course_type'
     
     name = fields.Char('Course Type',  required=True, help="The course type's name")
+    active = fields.Boolean('Active')
     objective = fields.Text('Objective',
                                   help="Allows to the user to write the objectives of the course type",
                                   translate=True,
@@ -52,36 +56,10 @@ class TrainingCourseType(ModelView, ModelSQL):
     description = fields.Text('Description',
                                     translate=True,
                                     help="Allows to the user to write the description of the course type")
-
-class TrainingCourseTheme(ModelView, ModelSQL):
-    'Course Theme'
-    __name__ = 'training.course.theme'
-
-    name = fields.Char('Name', required=True)
-    active = fields.Boolean('Active')
-    priority = fields.Integer('Priority')
-    description = fields.Text('Description',
-                                    help="Allows to write the description of the theme")
-
-    def default_action(self):
+    
+    @staticmethod
+    def default_active():
         return True
-    
-    def default_priority(self):
-        return 0
-
-class TrainingTemplateCourse(ModelView, ModelSQL):
-    'Training Template Course'
-    __name__ = 'training.template.course'
-    
-    name = fields.Char('Name')
-    category = fields.Many2One('training.course.category','Category', required=True)
-    format = fields.Many2One('training.format', 'Format')
-    force_format = fields.Boolean('Force Format')
-    language = fields.Char('Language')
-    force_language = fields.Boolean('Force Language') 
-    goal = fields.Text('Goal')
-    descrition = fields.Text('Description')
-    requirement = fields.Text('Requirement')
 
 class TrainingCourse(ModelView, ModelSQL):
     'Training Course'
@@ -92,8 +70,6 @@ class TrainingCourse(ModelView, ModelSQL):
             ('type', '=', 'service'), 
             ('category', '=', Id('courses'),
             )], select=True)
-    
-    splitted_by = fields.Char('Splitted by', required=True)
     duration = fields.Integer('Hours Duration', help='The hours duration of the course')
     parent = fields.Many2One('training.course',
                                  'Parent Course',
@@ -109,22 +85,22 @@ class TrainingCourse(ModelView, ModelSQL):
     category = fields.Many2One('training.course_category', 'Course Category')
     faculty = fields.Many2One('training.faculty', 'Faculty',
                                           help="The faculty who give the course")
-    internal_note = fields.Text('Note', translate=True, 
-                                help="The user can write some internal note for this course")
+    description = fields.Text('Description', translate=True, 
+                                help="The user can write some descritpion note for this course")
     state = fields.selection(STATE, 'State', required=True,
                                           help="The state of the course")
     active = fields.Boolean('Active')
     
     @staticmethod
-    def default_state(self):
+    def default_state():
         return 'draft'
     
     @staticmethod
-    def default_duration(self):
+    def default_duration():
         return 2
     
     @staticmethod
-    def default_active(self):
+    def default_active():
         return True
     
     @classmethod
@@ -133,17 +109,6 @@ class TrainingCourse(ModelView, ModelSQL):
         cls._sql_constraints += [
             ('name_uniq', 'UNIQUE(name)', 'The course must be unique.'),
             ]
-
-class TrainingOfferFormat(ModelView, ModelSQL):
-    'The delivery format of the offer or session'
-    __name__ = 'training.offer.format'
-
-    name = fields.Char('Name', required=True, help="The format's name of the offer")
-    active = fields.Boolean('Active')
-    
-    @staticmethod
-    def default_active(self):
-        return True
 
 class TrainingOffer(ModelView, ModelSQL):
     'Offer'
@@ -164,24 +129,21 @@ class TrainingOffer(ModelView, ModelSQL):
     management = fields.Text('Management',
                                     help="Allows to write the management of the course")
     sequence = fields.Integer('Sequence')
-    format = fields.Many2One('training.offer.format', 'Format', required=True,
-                                    help="Delivery format of the course")
     state = fields.Selection(STATE,
                               'State', required=True)
-    theme = fields.Many2One('training.course.theme', 'Theme')
     notification_note = fields.Text('Notification Note', help='This note will be show on notification emails')
     is_certification = fields.Boolean('Is a certification?', help='Indicate is this Offer is a Certification Offer')
 
     @staticmethod
-    def default_state(self):
+    def default_state():
         return 'draft'
     
     @staticmethod
-    def default_kind(self):
+    def default_kind():
         return 'standard'
     
     @staticmethod
-    def default_is_certification(self):
+    def default_is_certification():
         return True
 
 class TrainingCourseOfferRel(ModelSQL):
@@ -189,4 +151,4 @@ class TrainingCourseOfferRel(ModelSQL):
     __name__ = 'training.course.offer.rel'
 
     course = fields.Many2One('training.course', 'Course', required=True, ondelete='CASCADE')
-    offer = fields.Many2One('training.offer', 'Offer', required=True, ondelete='RESTRICT')  
+    offer = fields.Many2One('training.offer', 'Offer', required=True, ondelete='CASCADE')  
